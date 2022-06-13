7287b1564031f88f3988b3cc9f3c3e42c62869fd
Chicho <catania.gian@gmail.com>
2022-06-13 16:56:12 -0300

    Nuevos cambios

--- js/information.js
+++ js/information.js
@@ -1,15 +1,14 @@
-let hideText__btn = document.getElementById('hideText__btn');
-let hideText = document.getElementById('hideText');
+let hideText__btn = document.getElementById("hideText__btn");
+let hideText = document.getElementById("hideText");
 
-hideText__btn.addEventListener('click', toggleText);
+hideText__btn.addEventListener("click", toggleText);
 
 function toggleText() {
-    hideText.classList.toggle('showText');
-    
-    if(hideText.classList.contains('showText')) {
-        hideText__btn.innerHTML = 'MENOS INFORMACIÓN'
-    }
-    else {
-        hideText__btn.innerHTML = 'MÁS INFORMACIÓN'
-    }
+  hideText.classList.toggle("showText");
+
+  if (hideText.classList.contains("showText")) {
+    hideText__btn.innerHTML = "MENOS INFORMACIÓN";
+  } else {
+    hideText__btn.innerHTML = "MÁS INFORMACIÓN";
+  }
 }
